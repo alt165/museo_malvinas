d0c096de6945e006b6c4fa2d8766eda29b1e72b4
alt165 <alt165@gmail.com>
2026-05-01 12:29:52 -0300

    chore: add initial backend configuration

---
+++ backend/src/main/java/com/proveedores/MuseoBackendApplication.java
@@ -0,0 +1,12 @@
+package com.proveedores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuseoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MuseoBackendApplication.class, args);
+    }
+}
